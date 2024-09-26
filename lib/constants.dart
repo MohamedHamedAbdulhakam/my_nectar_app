c165486145595bd1019bdaded8fefaeeb3902170
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-26 22:08:51 +0300

    splash view part one

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const KeyPrimaryColor = Color(0xff53B175);
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:my_nectar_app/features/splash/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SplashViewBody();
+    return const SplashViewBody();
   }
 }
