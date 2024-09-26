fd2ef21465991ae88d6c4f4d3d540f562de0a49a
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-26 22:27:47 +0300

    finish splash view

--- lib/features/splash/widgets/splash_view_body.dart
+++ lib/features/splash/widgets/splash_view_body.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:my_nectar_app/constants.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: KeyPrimaryColor,
+      
+      child: Column(
+       mainAxisAlignment: MainAxisAlignment.center,
+       children: [
+        Image.asset('assets/Group 1.png',width: 200,height: 50,)
+       ],
+      ),
+    );
   }
 }
