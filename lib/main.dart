90fe9bd5f79d1faf20836e4ff2d92b32c97c68b8
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-28 15:52:29 +0300

    set debug show bunner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class NectarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp.router(
+      debugShowCheckedModeBanner: false,
 routerConfig: AppRouter.router,
      
     );
