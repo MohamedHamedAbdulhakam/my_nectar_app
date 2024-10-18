10eb2b66a3930e19bf48675ef4b08f756767dead
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-10-18 20:20:25 +0300

    home part one

--- lib/features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,10 +1,44 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
-  const HomeViewBody({super.key});
-
+   HomeViewBody({super.key});
+  final List<String> imagePaths = [
+    'assets/homeImage.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+    return CarouselSlider(      options: CarouselOptions(
+         autoPlay: true,
+         enlargeCenterPage: true,
+         aspectRatio: 16 / 9,
+         viewportFraction: 0.8,
+       ),
+       items: imagePaths.map((path) {
+         return Builder(
+           builder: (BuildContext context) {
+             return Image.asset(
+               path,
+               fit: BoxFit.cover,
+             );
+           },
+         );
+       }).toList(),
+     );
+   }
+ }
+
+
+
+// import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:flutter/material.dart';
+
+// class SingleImageCarousel extends StatelessWidget {
+//   final List<String> imagePaths = [
+//     'assets/banner (1).jpg',
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return 
