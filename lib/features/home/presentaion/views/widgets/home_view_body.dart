506914be7b28a73136c16193466d877b2fd7039f
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-10-21 22:27:08 +0300

    home view edit

--- lib/features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/features/home/presentaion/views/widgets/home_view_body.dart
@@ -32,35 +32,39 @@ class HomeViewBody extends StatelessWidget {
       body: Scaffold(
         body: Column(
           children: [
-           MaterialButton(
-            onPressed: () {
-              print("Search Store button pressed");
-            },
-            color: Colors.grey[200], // Background color
-            elevation: 0, // No shadow for a flat appearance
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0), // Rounded corners
-              side: BorderSide(color: Colors.blue), // Border
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0), // Button padding
-            child: Row(
-              mainAxisSize: MainAxisSize.min, // To wrap the button size around content
-              children: [
-                Icon(
-                  Icons.search, // Search icon
-                  color: Colors.black54,
-                ),
-                SizedBox(width: 10), // Spacing between icon and text
-                Text(
-                  'Search Store',
-                  style: TextStyle(
-                    color: Colors.black54, // Text color
-                    fontSize: 16.0,
+           Padding(
+             padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+             child: MaterialButton(
+              onPressed: () {
+                print("Search Store button pressed");
+              },
+              minWidth: double.infinity,
+              color: Colors.grey[200], // Background color
+              elevation: 0, // No shadow for a flat appearance
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0), // Rounded corners
+                // side: BorderSide(color: Colors.blue), // Border
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0), // Button padding
+              child: Row(
+                mainAxisSize: MainAxisSize.min, // To wrap the button size around content
+                children: [
+                  Icon(
+                    Icons.search, // Search icon
+                    color: Colors.black54,
                   ),
-                ),
-              ],
-            ),
-          ),
+                  SizedBox(width: 10), // Spacing between icon and text
+                  Text(
+                    'Search Store',
+                    style: TextStyle(
+                      color: Colors.black54, // Text color
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+                       ),
+           ),
       
             Expanded(
                 child: SingleChildScrollView(
