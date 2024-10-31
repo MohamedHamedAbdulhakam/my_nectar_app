a49149b9cc7e451f8a944a77e89d66f3bf85a0f4
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-10-31 22:58:01 +0300

    fix error

--- lib/features/explore/presentation/views/widgets/filter_action.dart
+++ lib/features/explore/presentation/views/widgets/filter_action.dart
@@ -1,92 +1,9 @@
-// import 'package:flutter/material.dart';
-// import 'package:my_nectar_app/constants.dart';
 
-// class FilterScreen extends StatefulWidget {
-//   @override
-//   _FilterScreenState createState() => _FilterScreenState();
-// }
-
-// class _FilterScreenState extends State<FilterScreen> {
-//   // Track selections for each category and brand
-//   Map<String, bool> categories = {
-//     'Eggs': true,
-//     'Noodles & Pasta': false,
-//     'Chips & Crisps': false,
-//     'Fast Food': false,
-//   };
-
-//   Map<String, bool> brands = {
-//     'Individual Collection': false,
-//     'Cocola': true,
-//     'Ifad': false,
-//     'Kazi Farmas': false,
-//   };
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Filters"),
-//         actions: [
-//           IconButton(
-//             icon: Icon(Icons.close),
-//             onPressed: () => Navigator.of(context).pop(),
-//           ),
-//         ],
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Expanded(
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//              const  Text("Categories", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-//               ...categories.keys.map((category) => CheckboxListTile(
-//                 controlAffinity: ListTileControlAffinity.leading,
-//                     title: Text(category),
-//                     value: categories[category],
-//                     activeColor: keyPrimaryColor,
-//                     onChanged: (value) {
-//                       setState(() {
-//                         categories[category] = value!;
-//                       });
-//                     },
-//                   )),
-//               SizedBox(height: 16),
-//               Text("Brand", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-//               ...brands.keys.map((brand) => CheckboxListTile(
-//                     title: Text(brand),
-//                     value: brands[brand],
-//                     activeColor: Colors.green,
-//                     onChanged: (value) {
-//                       setState(() {
-//                         brands[brand] = value!;
-//                       });
-//                     },
-//                   )),
-//               Spacer(),
-//               ElevatedButton(
-//                 style: ElevatedButton.styleFrom(
-//                   // primary: Colors.green,
-//                   minimumSize: Size(double.infinity, 50),
-//                 ),
-//                 onPressed: () {
-//                   // Apply filter logic here
-//                 },
-//                 child: Text("Apply Filter"),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
-import 'package:my_nectar_app/constants.dart';
-import 'package:my_nectar_app/core/utils/styles.dart';
-import 'package:my_nectar_app/core/widgets/custom_button.dart';
-import 'package:my_nectar_app/features/explore/presentation/views/widgets/custom_check_box_list_tile.dart';
+
+import '../../../../../core/utils/styles.dart';
+import '../../../../../core/widgets/custom_button.dart';
+import 'custom_check_box_list_tile.dart';
 
 class FilterAction extends StatefulWidget {
   const FilterAction({super.key});
