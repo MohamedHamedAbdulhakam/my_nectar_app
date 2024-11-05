43b48423bc60bbe60e94295b97c380b0101ecd45
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-05 16:12:11 +0200

    trying to solve the error

--- lib/features/my_cart/presentaion/views/widgets/cart_view_body.dart
+++ lib/features/my_cart/presentaion/views/widgets/cart_view_body.dart
@@ -5,37 +5,33 @@ import 'package:my_nectar_app/features/my_cart/presentaion/views/widgets/cart_it
 import 'package:my_nectar_app/features/my_cart/presentaion/views/widgets/checkout_button.dart';
 
 class CartViewBody extends StatefulWidget {
-   CartViewBody({super.key});
+  CartViewBody({super.key});
 
   @override
   State<CartViewBody> createState() => _CartViewBodyState();
 }
 
 class _CartViewBodyState extends State<CartViewBody> {
-    final List<CartItem> cartItems = [
-    CartItem(name: 'Bell Pepper Red', quantity: '1kg', price: 4.99, image: Assets.PepperImage),
-    CartItem(name: 'Egg Chicken Red', quantity: '4pcs', price: 1.99, image: Assets.VegetablesImage),
-    CartItem(name: 'Organic Bananas', quantity: '12kg', price: 3.00, image: Assets.actionDoneImage),
-    CartItem(name: 'Ginger', quantity: '250gm', price: 2.99, image: Assets.appleImage),
-    //   CartItem(name: 'Bell Pepper Red', quantity: '1kg', price: 4.99, image: Assets.PepperImage),
-    // CartItem(name: 'Egg Chicken Red', quantity: '4pcs', price: 1.99, image: Assets.VegetablesImage),
-    // CartItem(name: 'Organic Bananas', quantity: '12kg', price: 3.00, image: Assets.actionDoneImage),
-    // CartItem(name: 'Ginger', quantity: '250gm', price: 2.99, image: Assets.appleImage),
+  final List<CartItem> cartItems = [
+    CartItem(
+        name: 'Bell Pepper Red',
+        quantity: '1kg',
+        price: 4.99,
+        image: Assets.PepperImage),
+    // Add more items as needed...
   ];
 
   void removeItem(int index) {
-  if (index >= 0 && index < cartItems.length) {
     setState(() {
       cartItems.removeAt(index);
     });
   }
-}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Cart',style:Styles.textStyle26,),
+        title: const Text('My Cart', style: Styles.textStyle26),
         centerTitle: true,
       ),
       body: Column(
@@ -44,22 +40,26 @@ class _CartViewBodyState extends State<CartViewBody> {
             child: ListView.builder(
               itemCount: cartItems.length,
               itemBuilder: (context, index) {
-                return CartItemWidget(cartItem: cartItems[index],widget:  IconButton(
-            icon: Icon(Icons.close),
-           onPressed: () => removeItem(index),
-          ), );
+                return CartItemWidget(
+                  cartItem: cartItems[index],
+                  widget: IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () => removeItem(index),
+                  ),
+                );
               },
             ),
           ),
-          CheckoutButton(totalPrice: 12.96),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: CheckoutButton(totalPrice: calculateTotalPrice()),
+          ),
         ],
       ),
     );
   }
+
+  double calculateTotalPrice() {
+    return cartItems.fold(0, (sum, item) => sum + item.price);
+  }
 }
-
-
-
-
-
-  
